7733c745bc8bb97de86cc89bcb274b4864993343
Creeger <magnus.tysdal@gmail.com>
2024-04-11 13:48:37 +0200

    Endret TF_IDF til å være mer generell

--- BoW.py
+++ BoW.py
@@ -6,10 +6,11 @@ def getCountVector(df):
     vector = vectoriser.fit_transform(df["abstract"])
     return vector
 
-def TF_IDF(dataFrame):
-	vectorizer = TfidfVectorizer()
+def TF_IDF(dataFrame, column):
+	vectorizer = TfidfVectorizer(stop_words="english")
 	
-	X = vectorizer.fit_transform(dataFrame['abstract'])
+	X = vectorizer.fit_transform(dataFrame[column])
 
 	Y = vectorizer.get_feature_names_out()
 	return X, Y
+
