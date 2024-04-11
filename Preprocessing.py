67b4a4120c802d5b1a3cc6b53825f3dc3b71f670
Creeger <magnus.tysdal@gmail.com>
2024-04-11 12:29:30 +0200

    La til to  hjelpe funksjoner for TF_IDF

--- Preprocessing.py
+++ Preprocessing.py
@@ -7,3 +7,21 @@ def cleanAbstract(abstract):
 
 def sortFrameByID(dataFrame):
 	return dataFrame.sort_values(by=['id'])
+
+
+
+#Threshhold er hvor høy TF_IDF scoren må være for å bli printet
+def printDocumentWords(WordDocDF, docIdx, threshhold, labels):
+	print(labels[docIdx])
+	for i in range(WordDocDF.shape[docIdx]):
+		if WordDocDF[docIdx].iloc[i] > threshhold:
+			label = WordDocDF.iloc[[i]].index.values[docIdx]
+			print(f"Word: {label}, TF_IDF: {WordDocDF[0].iloc[i]}")
+
+
+def dropEnglishWords(dataFrame, X, Y, wordsToRemove):
+	for i in range(X.shape[1]):
+	#print(Y[i])
+		if Y[i] in wordsToRemove:
+			dataFrame = dataFrame.drop(index=Y[i])
+	return dataFrame
