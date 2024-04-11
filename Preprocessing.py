a91a2d6343cd28b8c924624797a7ed2fa3b6d65b
Jesper <j.soberg@stud.uis.no>
2024-04-11 11:27:34 +0200

    Konverterer labels til vektorer

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,4 +1,5 @@
 import string
+import torch
 
 def cleanAbstract(abstract):
 	abstract = abstract.lower()
@@ -7,3 +8,22 @@ def cleanAbstract(abstract):
 
 def sortFrameByID(dataFrame):
 	return dataFrame.sort_values(by=['id'])
+
+def getVectorLabels(dataframe):
+	labelLookup = {
+		"eess": 	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		"quant-ph": [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		"physics": 	[0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+		"stat": 	[0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+		"math": 	[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+		"astro-ph": [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+		"cond-mat": [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+		"hep-th": 	[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+		"cs": 		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+		"hep-ph": 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	}
+	result = []
+	for label in dataframe["label"]:
+		result.append(labelLookup[label])
+
+	return torch.tensor(result, dtype=torch.float)
