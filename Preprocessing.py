5425298ba27887c662225429ae79cbb754bc68b2
Creeger <magnus.tysdal@gmail.com>
2024-04-12 16:35:19 +0200

    Laget funksjon for lesing og rensing av data

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,14 +1,11 @@
 import string
 import torch
+import pandas as pd
 
 def cleanAbstract(abstract):
 	abstract = abstract.lower()
 	translator = str.maketrans("\n", " ", string.punctuation)
 	return abstract.translate(translator)
-
-def sortFrameByID(dataFrame):
-	return dataFrame.sort_values(by=['id'])
-
 
 
 #Threshhold er hvor høy TF_IDF scoren må være for å bli printet
@@ -20,12 +17,6 @@ def printDocumentWords(WordDocDF, docIdx, threshhold, labels):
 			print(f"Word: {label}, TF_IDF: {WordDocDF[0].iloc[i]}")
 
 
-def dropEnglishWords(dataFrame, X, Y, wordsToRemove):
-	for i in range(X.shape[1]):
-	#print(Y[i])
-		if Y[i] in wordsToRemove:
-			dataFrame = dataFrame.drop(index=Y[i])
-	return dataFrame
 def getVectorLabels(dataframe):
 	labelLookup = {
 		"eess": 	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -44,3 +35,13 @@ def getVectorLabels(dataframe):
 		result.append(labelLookup[label])
 
 	return torch.tensor(result, dtype=torch.float)
+
+
+def getDataFrameFromData(data):
+	dataFrame = pd.read_csv(data, nrows=100)
+	#labels = dataFrame["label"].unique()
+	labels = getVectorLabels(dataFrame)
+
+	dataFrame = dataFrame.map(lambda cell: cleanAbstract(str(cell)))
+
+	return dataFrame, labels
