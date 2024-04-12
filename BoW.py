7ce24aa3cb0b60f37b067b972b38ab9aa7f02eab
Jesper <j.soberg@stud.uis.no>
2024-04-12 10:42:19 +0200

    countvector gir nå en tensor og et ordforråd

--- BoW.py
+++ BoW.py
@@ -1,10 +1,28 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from scipy.sparse import coo_matrix
+
+import numpy as np
+import torch
 
 
-def getCountVector(df):
-    vectoriser = CountVectorizer()
+def getCountVector(df, vocabulary=None):
+    vectoriser = CountVectorizer(vocabulary=vocabulary)
     vector = vectoriser.fit_transform(df["abstract"])
-    return vector
+    tensor = countVector2Tensor(vector)
+    return tensor, vectoriser.vocabulary_
+
+def countVector2Tensor(vector):
+	coo = coo_matrix(vector)
+
+	values = coo.data
+	indices = np.vstack((coo.row, coo.col))
+
+	i = torch.LongTensor(indices)
+	v = torch.FloatTensor(values)
+	shape = coo.shape
+
+	tensor = torch.sparse.FloatTensor(i, v, torch.Size(shape))
+	return tensor
 
 def TF_IDF(dataFrame, column):
 	vectorizer = TfidfVectorizer(stop_words="english")
