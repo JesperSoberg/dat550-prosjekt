d8f274b7b9f33d6f49ed956271afa15240f1a45d
Creeger <magnus.tysdal@gmail.com>
2024-04-12 16:32:58 +0200

    Oppdatert rnn klasse

--- rnn.py
+++ rnn.py
@@ -1,17 +1,13 @@
 import torch
 import torch.nn as nn
 
-import random
-
-
-seed = 42
 
 class RNN(nn.Module):
 	def __init__(self, inputSize, hiddenSize, numLayers, rnnType="rnn"):
 		super(RNN, self).__init__()
 		self.hiddenSize = hiddenSize
 		self.numLayers=numLayers
-		self.rnn = nn.RNN(input_size=inputSize, hidden_size=hiddenSize, num_layers=numLayers, batch_first=True)
+		self.rnn = nn.RNN(input_size=inputSize, hidden_size=hiddenSize, num_layers=numLayers, dropout=0.5)
 
 		self.fc = nn.Linear(hiddenSize, 10)
 		self.softmax = nn.Softmax(dim=1)
@@ -34,24 +30,3 @@ class RNN(nn.Module):
 
 		
 		return out
-			
-
-def train(trainModel, optimizer="adam", model_type="RNN", x=None, labels=None):
-	print("Training...")
-	data = x
-	criterion = nn.CrossEntropyLoss()#BCELoss()
-	learnRate = 0.01
-
-	optimizer = torch.optim.Adam(trainModel.parameters(), lr=learnRate)
-
-	numEpochs = 100
-	for epoch in range(numEpochs):
-		prediction = trainModel(data)
-		loss = criterion(prediction, labels)
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-		if epoch % 10 == 0:  # Print every 10 epochs
-			print(f'Epoch {epoch}, Loss: {loss.item()}')
-
-	return trainModel
