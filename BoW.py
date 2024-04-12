2d761a7cd2dd1a759b51f71f770d5b3cbf9b40f6
Creeger <magnus.tysdal@gmail.com>
2024-04-12 10:44:14 +0200

    Oppdatert stop_words liste

--- BoW.py
+++ BoW.py
@@ -7,7 +7,9 @@ def getCountVector(df):
     return vector
 
 def TF_IDF(dataFrame, column):
-	vectorizer = TfidfVectorizer(stop_words="english")
+	vectorizer = TfidfVectorizer(stop_words=['an', 'it', 'in', 'and', 'all', 'and', 'was', 'the', 'of', 'more', 'than',
+			   'are', 'for', 'to', 'which', 'is', 'its', 'that', 'two', 'when',
+			   'our'])
 	
 	X = vectorizer.fit_transform(dataFrame[column])
 
